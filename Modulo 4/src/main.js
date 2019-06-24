f99b50bb249127443df774bf336bbd663657df0b
diegotesch <diego.tesch@gmail.com>
2019-06-24 11:52:49 -0300

    Buscando da API

--- Modulo 4/src/main.js
+++ Modulo 4/src/main.js
@@ -1,8 +1,11 @@
+import api from './api';
+
 class App{
     constructor(){
         this.repositories = [];
 
         this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]');
         this.listEl = document.getElementById('repo-list');
 
         this.registerHandlers();
@@ -13,17 +16,32 @@ class App{
         this.formEl.onsubmit = event => this.addRepository(event);
     }
 
-    addRepository(event){
+    async addRepository(event){
         event.preventDefault();
 
+        const repoInput = this.inputEl.value;
+
+        //caso não haja nada escrito para a execução da funcao
+        if(repoInput.length === 0)
+            return;
+
+        const response = await api.get(`/repos/${repoInput}`);
+
+        //utilizando a desestruturação, remove os elementos necessários do objeto
+        const { name, description, html_url, owner: {  avatar_url } } = response.data;
+
+        //utilizando as shortsyntax passa as variaveis retiradas para os elementos
         this.repositories.push({
-            name: 'rocketseat.com.br',
-            description: 'Tire a seua ideia do papel e de vida a sua startup.',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-            html_url: 'http://github.com/rocketseat/unform'
+            name,
+            description,
+            avatar_url,
+            html_url
         });
 
+        this.inputEl.value = '';
+
         this.render();
+
     }
 
     render(){

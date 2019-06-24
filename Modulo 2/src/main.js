84759f4e0f4f0ba4eff897ad18495002cee51dd5
diegotesch <diego.tesch@gmail.com>
2019-06-24 09:40:52 -0300

    Desafio Modulo 3

--- Modulo 2/src/main.js
+++ Modulo 2/src/main.js
@@ -1,18 +1,62 @@
-import axios from 'axios';
+import axios from "axios";
 
-class Api{
-    static async getUserInfo(username){
-        try{
-            const response = await axios.get(`https://api.github.com/users/${username}`);
+//Exercicio 1
+// const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
 
-            console.log(response);
-        }catch (err){
-            console.log('Erro na API');
-        }
-        
-        
+// async function umPorSegundo(){
+//     await delay();
+//     console.log('1s');
+//     await delay();
+//     console.log('2s');
+//     await delay();
+//     console.log('3s');
+// }
+
+// umPorSegundo();
+
+//Exercicio 2
+// async function getUserFromGithub(user){
+//     try{
+//         const res = await axios.get(`https://api.github.com/users/${user}`);
+
+//         console.log(res.data);
+//     }catch(err){
+//         console.log('Erro na API');
+//     }
+// }
+
+// getUserFromGithub('diegotesch');
+// getUserFromGithub('dtesch');
+// getUserFromGithub('dteschda3s3');
+
+//Exercicio 3
+// class Github{
+//     static async getRepositories(respo){
+//         try{
+//             const res = await axios.get(`https://api.github.com/repos/${respo}`);
+//             console.log(res.data);
+//         }catch(err){
+//             console.log('Repositório não encontrado');
+//         }
+//     }
+// }
+
+// Github.getRepositories('rocketseat/rocketseat.com.br');
+// Github.getRepositories('rocketseat/dlkvmsk');
+// Github.getRepositories('diegotesch/Estudos-ES6');
+// Github.getRepositories('diegotesch/estudos');
+// Github.getRepositories('diegotesch/blabla');
+
+//Exercicio 4
+const buscaUsuario = async user => {
+    try{
+        const res = await axios.get(`https://api.github.com/users/${user}`);
+        console.log(res);
+    }catch(err){
+        console.log('Usuário não existe');
     }
 }
 
-Api.getUserInfo('diegoteschfeswsw');
-Api.getUserInfo('diegotesch');
+buscaUsuario('diegotesch');
+buscaUsuario('diego3g');
+buscaUsuario('didaeego3g');
---
+++ Modulo 2/src/main_v1.js
@@ -0,0 +1,32 @@
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve('OK') }, 2000);
+});
+
+//MODELO ANTIGO - DEPRECIADO
+// minhaPromise()
+//     .then(response => {
+//         console.log(response);
+//     })
+//     .catch(err => {
+
+//     });
+
+//Obs: Await sempre seve ser utilizado dentro de uma função asincrona
+// async function executaPromise(){
+//     console.log( await minhaPromise() );
+//     console.log( await minhaPromise() );
+//     console.log( await minhaPromise() );
+//     console.log( await minhaPromise() );
+//     console.log( await minhaPromise() );
+// }
+
+//exemplo acima com arrow functions
+const executaPromise = async () => {
+    console.log( await minhaPromise() );
+    console.log( await minhaPromise() );
+    console.log( await minhaPromise() );
+    console.log( await minhaPromise() );
+    console.log( await minhaPromise() );
+}
+
+executaPromise();
